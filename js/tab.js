dbf279fa34c2d199f9dde2dcf5d87497fc0f77c5
IwataEnterPrise <145575759+IwataEnterPrise@users.noreply.github.com>
2023-10-19 11:35:16 +0900

    Update tab.js

--- js/tab.js
+++ js/tab.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // 初期状態で最初のタブを表示
     $("#tab1").addClass("active");
 
     // タブボタンがクリックされたときの処理
-    $(".tab-button").click(function() {
+    $(".tab-button").click(function () {
         // クリックされたボタンのデータ属性から対応するタブを取得
-        var tabId = $(this).data("tab");
-
-        // すべてのタブコンテンツを非表示にし、クリックされたタブを表示
-        $(".tab-panel").removeClass("active");
-        $("#" + tabId).addClass("active");
+        const tabId = $(this).data("tab");
 
         // タブボタンのアクティブ状態を切り替え
         $(".tab-button").removeClass("active");
         $(this).addClass("active");
+
+        // タブコンテンツのフェードアウト
+        $(".tab-panel.active").fadeOut(300, function () {
+            // フェードアウトが完了したら、新しいタブコンテンツを表示
+            $(".tab-panel").removeClass("active");
+            $("#" + tabId).addClass("active");
+            // タブコンテンツのフェードイン
+            $("#" + tabId).fadeIn(300);
+        });
     });
 });
